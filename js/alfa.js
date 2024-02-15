739707ff09946bde3eac5b70012504f9c6e93ca2
Md Mofijul Haq Sikder <mofijulhoq280382@gmail.com>
2024-02-15 23:15:59 +0600

    Add Change screen home to play ground

---
+++ js/alfa.js
@@ -0,0 +1,8 @@
+function playNow(){
+    // const homeSection = document.getElementById('home-screen').classList;
+    // homeSection.add('hidden');
+    // const playSection= document.getElementById('play-screen').classList;
+    // playSection.remove('hidden');
+    hideElementById('home-screen');
+    showElementById('play-screen');
+}
